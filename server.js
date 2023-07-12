86a051421c453158f293e97863205169ba90318c
ivar246 <ravistha869@gmail.com>
2023-07-12 22:50:27 +0545

    ...

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mailRoute = require("./route/mail");
+const mailRoute = require("./routes/mail");
+const { google } = require("googleapis")
 const app = express();
 
 app.use(bodyParser.json());
 app.use(mailRoute);
+
+
 
 app.listen(3000, () => {
   console.log("server listening on port 3000");
